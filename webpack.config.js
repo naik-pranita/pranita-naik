a8f4113ff15bfb4cb90f46ed09a77741cca95ac9
Pranita <pnaik93@gmail.com>
2018-02-22 22:39:22 +0530

    serving files from build folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,34 @@
 var PATH = require('path');
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var baseSrcPath = PATH.resolve(__dirname + '/app/src');
 var outputPath = PATH.resolve(__dirname + '/public/');
 
 
 module.exports = {
-    entry : {
-        app: baseSrcPath + '/app.js'
+    entry: {
+        app: ['babel-polyfill', PATH.join(baseSrcPath + '/app.js')]
     },
     output: {
         filename: '[name].bundle.js',
-        path: outputPath
+        path: PATH.join(outputPath + '/build/')
     },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './public/index.html' })
+    ],
     module: {
         loaders: [{
             test: /\.js$/,
             exclude: /node_modules/,
-            loader: 'babel-loader'
+            loader: 'babel-loader',
+            options: {
+                compact: true
+            }
         }]
+    },
+    resolve: {
+        extensions: ['.js', '.es6']
     }
 }
 
